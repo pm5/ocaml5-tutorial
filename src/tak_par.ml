1050b88712b5289f3120c38325b6619b28f87695
Pomin Wu <pomin5@protonmail.com>
2024-07-29 22:42:13 +0800

    feat: tak

--- src/tak_par.ml
+++ src/tak_par.ml
@@ -1,20 +1,25 @@
 module T = Domainslib.Task
 
-let (num_domains,x,y,z) =
+let num_domains, x, y, z =
   try
     let num_domains = int_of_string Sys.argv.(1) in
     let x = int_of_string Sys.argv.(2) in
     let y = int_of_string Sys.argv.(3) in
     let z = int_of_string Sys.argv.(4) in
-    (num_domains,x,y,z)
-  with _ -> (1,18,12,6)
+    (num_domains, x, y, z)
+  with _ -> (1, 18, 12, 6)
 
 let rec tak x y z =
-  if x > y then
-    tak (tak (x-1) y z) (tak (y-1) z x) (tak (z-1) x y)
-  else z
+  if x > y then tak (tak (x - 1) y z) (tak (y - 1) z x) (tak (z - 1) x y) else z
 
-let rec tak_par p x y z = failwith "not implemented"
+let rec tak_par p x y z =
+  if x < 20 && y < 20 then tak x y z
+  else if x > y then
+    let x' = T.async p (fun () -> tak_par p (x - 1) y z)
+    and y' = T.async p (fun () -> tak_par p (y - 1) z x)
+    and z' = T.async p (fun () -> tak_par p (z - 1) x y) in
+    tak_par p T.(await p x') T.(await p y') T.(await p z')
+  else z
 
 let main () =
   let p = T.setup_pool ~num_domains:(num_domains - 1) () in
