d08bf69e9d7a940d0a8721b2ec5d63b89830377f
Pomin Wu <pomin5@protonmail.com>
2024-07-29 17:48:05 +0800

    feat: fib

--- src/fib_par.ml
+++ src/fib_par.ml
@@ -4,8 +4,11 @@ let rec fib n = if n < 2 then 1 else fib (n - 1) + fib (n - 2)
 
 let fib_par n =
   if n > 20 then begin
-    (* Only use parallelism when problem size is large enough *)
-    failwith "not implemented"
+    let d1 = Domain.spawn (fun _ -> fib (n - 1))
+    and d2 = Domain.spawn (fun _ -> fib (n - 2)) in
+    let r1 = Domain.join d1
+    and r2 = Domain.join d2 in
+    r1 + r2
   end else fib n
 
 let main () =
